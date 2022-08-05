c59eae62d7bab8d95698a3c84b6d027cf1569e8c
donia sameh <doniasameh2001@gmail.com>
2022-08-05 03:45:22 +0200

    scanning double second number trial

--- src/main/kotlin/mainFile.kt
+++ src/main/kotlin/mainFile.kt
@@ -1,10 +1,17 @@
 import java.util.Scanner
 
 fun main(){
-    val version = " this is version 1"
+    val version = " this is version 2"
     println(version)
 
     var scan = Scanner(System.`in`)
-    println("enter first number")
-    var number1 : Int = scan.nextInt();
+    println("enter the first number")
+    var number1 : Double = scan.nextDouble()
+
+    println("enter the second number")
+    var number2 : Double = scan.nextDouble()
+
+
+
+
 }
