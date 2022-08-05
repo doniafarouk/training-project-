98e38f5a8021e19b1079985cacce8a8e53b06134
donia sameh <doniasameh2001@gmail.com>
2022-08-05 03:10:02 +0200

    scanning first number trial

---
+++ src/main/kotlin/mainFile.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main(){
+    val version = " this is version 1"
+    println(version)
+
+    var scan = Scanner(System.`in`)
+    println("enter first number")
+    var number1 : Int = scan.nextInt();
+}
